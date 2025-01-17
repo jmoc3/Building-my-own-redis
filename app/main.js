c6d3dd7e3112112592c1834bd3a1c7c06323edcf
orejarena <jmoc951@gmail.com>
2025-01-17 10:47:55 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -5,9 +5,17 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-    connection.on("data", ()=>{
-        connection.write("+PONG\r\n")
+
+    connection.on("data", (data)=>{
+        if(data.toString().toLowerCase().startsWith("echo")){
+            connection.write(`${data.toString().slice(5,-1)}\r\n`)
+        }
     })
+
+    connection.on("end", ()=>{
+        console.log("Someone out")
+    })
+
 });
 
 server.listen(6379, "127.0.0.1", ()=>{
