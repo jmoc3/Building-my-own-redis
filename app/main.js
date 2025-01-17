b473e28de1080c760b4918c64de2093591accdda
orejarena <jmoc951@gmail.com>
2025-01-17 17:49:00 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -26,7 +26,7 @@ const server = net.createServer((connection) => {
         const getTrue = inputArray[2] == "get"
 
         if (setTrue) {
-            data[inputArray[4]] = inputArray[5]
+            data[inputArray[4]] = inputArray[4]
             return connection.write("+OK\r\n")
         }
         if (getTrue) return connection.write(`$${data[inputArray[4]].length}\r\n${data[inputArray[4]]}\r\n`)
