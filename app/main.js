29c874530ea0cd56f4a228907adf595dbbefbc73
orejarena <jmoc951@gmail.com>
2025-01-17 17:29:35 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
 
     connection.on("data", (data)=>{
-
+        console.log(data.toString())
         if (data.toString()=="PING") return connection.write("$2\r\nPONG\r\n")
 
         const input = Buffer.from(data).toString().toLowerCase()
