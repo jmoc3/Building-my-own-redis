de18ac590451737282c4e57f120a50e826dac2a5
orejarena <jmoc951@gmail.com>
2025-01-19 18:28:20 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -44,7 +44,7 @@ const server = net.createServer((connection) => {
           data.push(String.fromCharCode(`${e}`))
         }
       })
-      console.log(typeof fileDataFiltered[3])
+      console.log(fileDataFiltered[3]+4)
       console.log(data)
       // file.map(hex =>{
       //   console.log(hex)
