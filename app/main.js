7a8fe3ec7d4db2b7fe90203350fcbfb5a3cee869
orejarena <jmoc951@gmail.com>
2025-01-20 16:23:56 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -18,9 +18,7 @@ const server = net.createServer((connection) => {
   connection.on("data", (clientInput)=>{
 
     if(config["dir"]!=null){
-      const existFile = fs.existsSync(path)
-      if(existFile) return connection.write("+THE PATH JUST DOESN'T EXISTS\r\n")
-        
+      
       const file = fs.readFileSync(`${config["dir"]}/${config["dbfilename"]}`)
       // const file = fs.readFileSync(`/home/jmoc/Desktop/codecrafters-redis-javascript/app/regular_set.rdb`)
       let fbFound = false
