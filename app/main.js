8b570604dbcb33550f74a7e35af671a89cc62c9c
orejarena <jmoc951@gmail.com>
2025-01-20 09:37:17 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -20,7 +20,7 @@ const server = net.createServer((connection) => {
       // const file = fs.readFileSync(`/home/jmoc/Desktop/codecrafters-redis-javascript/app/regular_set.rdb`)
       
       for(i=0;i<file.length;i++){
-        console.log(String.fromCharCode(file[i]))
+        console.log(file[i].toString(16).padStart(2,"0"))
       }
 
     }
