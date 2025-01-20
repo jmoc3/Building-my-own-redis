1c95f2cbc02e04ca330bb4c88fed2b9e62a1297b
orejarena <jmoc951@gmail.com>
2025-01-20 15:38:13 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -132,7 +132,7 @@ const server = net.createServer((connection) => {
       const lenKeyWords = keyWords.map(e => e.length) 
       let res = ""
       for(i=0;i>keyWords.length;i++){
-        res += `$${lenKeyWords[i].length}\r\n${keyWords[i]}`
+        res += `$${lenKeyWords[i]}\r\n${keyWords[i]}`
       }
       console.log(res)
       return connection.write("+OK\r\n")
