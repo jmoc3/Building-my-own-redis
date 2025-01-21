7f186465a532275e51d48bab8c6f3e26d97f4339
orejarena <jmoc951@gmail.com>
2025-01-21 16:06:24 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -50,17 +50,17 @@ const server = net.createServer((connection) => {
           continue
         }
         
-        if(hexValue=="fc"){ indexExpirityEnd = i+9; continue} 
+        if(hexValue=="fc"){ indexExpirityEnd = i+8; continue} 
         
-        if(i<indexExpirityEnd){
+        if(i<=indexExpirityEnd){
           expirity += hexValue
           continue
         }
 
-        if(i==indexExpirityEnd) {pair[2] = expirity; expirity = ""; continue}
+        if(i==(indexExpirityEnd+1)) {pair[2] = expirity; expirity = ""}
         
         console.log(hexValue, String.fromCharCode(file[i]), pair,stringFounded)
-        if(file[i]=="00") { stringFounded = true ; continue}
+        if(hexValue=="00") { stringFounded = true ; continue}
         
         if (stringFounded) {
           sizeString[0] = String.fromCharCode(file[i]).charCodeAt(0)
