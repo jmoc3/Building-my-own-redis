f8630f41a521e84ecaaf259ce2691d624815446f
orejarena <jmoc951@gmail.com>
2025-01-22 13:31:07 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -81,8 +81,10 @@ const server = net.createServer((connection) => {
             pair[1] = keyString 
             pair[2] = new Date(Number(BigInt("0x" + pair[2].match(/../g).reverse().join(""))))
 
-            console.log(Date.now() > pair[2], pair[2])
-            storage[pair[0]] = {"value":pair[1], "expirity":pair[2]}
+            console.log(Date.now() < pair[2], pair[2])
+            if(Date.now() < pair[2]){
+              storage[pair[0]] = {"value":pair[1], "expirity":pair[2]}
+            }
             pair=[] 
           }
           
