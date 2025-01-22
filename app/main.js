76721b43d7be14c749281347d290410ada83fe4a
orejarena <jmoc951@gmail.com>
2025-01-22 13:16:48 -0500

    [any message]

--- app/main.js
+++ app/main.js
@@ -79,7 +79,7 @@ const server = net.createServer((connection) => {
           if (pair[0]==undefined) { pair[0] = keyString }
           else { 
             pair[1] = keyString 
-            console.log(BigInt("0x" + pair[2].split("").reverse().join("")))
+            console.log(Number(BigInt("0x" + pair[2].split("").reverse().join(""))))
             storage[pair[0]] = {"value":pair[1], "expirity":pair[2]}
             pair=[] 
           }
