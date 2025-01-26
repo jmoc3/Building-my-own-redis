9ff7fe40fd4dde59d3345ac5429d179ac250329e
orejarenam <jmoc951@gmail.com>
2025-01-26 14:21:20 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -212,15 +212,11 @@ const server = net.createServer((connection) => {
     if(psync){
 
       connection.write(`+FULLRESYNC ${config["info"]["replication"]["master_replid"]} ${config["info"]["replication"]["master_repl_offset"]}\r\n`)
-      const hexfile = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa231fa0972656469732e30fa0a72656469fa056374696d65c265642d6d656dc2b02d62617365c000ff"
-      const hexFileArray = hexfile.match(/../g)
-      const binaryFile = hexFileArray.map(e=>parseInt(e,16).toString(2).padStart(8,'0'))
-      console.log(binaryFile)
-      const fileContent = binaryFile.join("")
-
-      connection.write(`$${fileContent.length}\r\n`) 
-      connection.write(`${fileContent}`) 
-      return
+      const base = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
+      const buffer = Buffer.from(base,"base64")
+      const bufferHeader = Buffer.from(`$${buffer.length}\r\n`)  
+       
+      return connection.write(Buffer.concat(bufferHeader,buffer))
     }
 
     // ECHO configuration
