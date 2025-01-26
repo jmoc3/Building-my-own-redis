859f9a04b604d3c3e768baf058d90f4d6708c0e9
orejarenam <jmoc951@gmail.com>
2025-01-26 13:46:16 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -210,7 +210,10 @@ const server = net.createServer((connection) => {
     // PSYNC configuration
     const psync = inputArray[2] == "psync"
     if(psync){
-      return connection.write(`+FULLRESYNC ${config["info"]["replication"]["master_replid"]} ${config["info"]["replication"]["master_repl_offset"]}\r\n`)
+
+      connection.write(`+FULLRESYNC ${config["info"]["replication"]["master_replid"]} ${config["info"]["replication"]["master_repl_offset"]}\r\n`)
+      const hexfile = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+      return Buffer.from(hexfile,"hex")
     }
 
     // ECHO configuration
