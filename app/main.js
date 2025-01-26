1057b9a77a05cd3aa16017c5c5181e2826f0a50f
orejarenam <jmoc951@gmail.com>
2025-01-26 13:50:14 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -212,8 +212,10 @@ const server = net.createServer((connection) => {
     if(psync){
 
       connection.write(`+FULLRESYNC ${config["info"]["replication"]["master_replid"]} ${config["info"]["replication"]["master_repl_offset"]}\r\n`)
-      const hexfile = "$524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-      return hexfile
+      const hexfile = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+      const hexFileArray = hexfile.match(/../g)
+      console.log(hexfile)
+      return 
     }
 
     // ECHO configuration
