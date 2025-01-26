8fb6c3875e4efdec0568e27706b232409fabc456
orejarenam <jmoc951@gmail.com>
2025-01-26 12:56:30 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -222,9 +222,10 @@ server.listen(config["port"], "127.0.0.1", ()=>{
       })
     
       master.on("data",(data)=>{
-        console.log(data.toString())
+        console.log("Master Data Resp: " + data.toString())
         master.write("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n")
         master.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
+        master.write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
        
         master.end()
       })
