6d1f300b2cc86e8718b03571d759a0129c2e171b
orejarenam <jmoc951@gmail.com>
2025-01-26 13:06:04 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -218,15 +218,11 @@ server.listen(config["port"], "127.0.0.1", ()=>{
       const masterConf = process.argv[replicaofId + 1].split(" ")
       const master = net.createConnection({host:masterConf[0], port:masterConf[1]}, ()=>{
         console.log("Connected to master")
+        master.write("*1\r\n$4\r\nPING\r\n")
+        setTimeout(()=>{ master.write("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n") },1000)
+        setTimeout(()=>{ master.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n") },2000)
+        setTimeout(()=>{ master.write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n") },3000)
       })
-      master.write("*1\r\n$4\r\nPING\r\n")
-      master.write("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n")
-      master.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
-      master.write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
 
-      // master.on("data",(data)=>{
-      //   console.log("Master Data Resp: " + data.toString() + " " + data.toString()!=undefined)
-
-      // })
     }
 });
