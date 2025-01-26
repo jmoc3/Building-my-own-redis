78a2b44ca6f02aba12c3ecb957d1578e6cc92584
orejarenam <jmoc951@gmail.com>
2025-01-26 14:01:04 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -215,7 +215,9 @@ const server = net.createServer((connection) => {
       const hexfile = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
       const hexFileArray = hexfile.match(/../g)
       const binaryFile = hexFileArray.map(e=>parseInt(e,16).toString(2))
-      return connection.write(`$${binaryFile.join("").length.toString(2)}\r\n${binaryFile.join("")}`) 
+      const fileContent = binaryFile.join("")
+
+      return connection.write(`$${fileContent.length}\r\n${fileContent}`) 
     }
 
     // ECHO configuration
