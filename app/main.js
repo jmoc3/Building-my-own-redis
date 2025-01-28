5175c00082af7819ccc5b36796ccbcde18e6a20a
orejarenam <jmoc951@gmail.com>
2025-01-28 14:57:55 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -316,7 +316,7 @@ const server = net.createServer((connection) => {
     // WAIT configuration
     const wait = inputArray[2] == "wait"
     if(wait){
-      return `:${inputArray[4]}\r\n`
+      return connection.write(`:${inputArray[4]}\r\n`)
     }
 
     // Default response to something wrong
