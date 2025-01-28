c1673922e31634505b1b68a6a76a0ade63df8d44
orejarenam <jmoc951@gmail.com>
2025-01-28 13:45:04 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -88,8 +88,9 @@ if(replicaofBool){
 
     const getackfId = inputArray.indexOf("getack")
     if (getackfId){
+      master.write(`*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$${config["info"]["replication"]["master_repl_offset"].toString().length}\r\n${config["info"]["replication"]["master_repl_offset"]}\r\n`)
       config["info"]["replication"]["master_repl_offset"]+=37
-      return master.write(`*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$${config["info"]["replication"]["master_repl_offset"].toString().length}\r\n${config["info"]["replication"]["master_repl_offset"]}\r\n`)
+      return 
     }
     
     // Default response to something wrong
