7f1542349d5e318adcf51a741b2595ea80301b0c
orejarenam <jmoc951@gmail.com>
2025-01-28 15:15:23 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -92,7 +92,7 @@ if(replicaofBool){
     }
 
     const getackfId = inputArray.indexOf("getack")
-    console.log(inputArray)
+    console.log(input, input.indexOf("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n*\r\n"))
     if (getackfId!=-1){
       master.write(`*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$${config["info"]["replication"]["master_repl_offset"].toString().length}\r\n${config["info"]["replication"]["master_repl_offset"]}\r\n`)
       config["info"]["replication"]["master_repl_offset"]+=37 
