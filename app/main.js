3552238784471fa7116718e73f60b75dc6d51f16
orejarenam <jmoc951@gmail.com>
2025-01-28 13:39:42 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -50,7 +50,7 @@ if(replicaofBool){
 
     const input = data.toString().toLowerCase()
     const inputArray =  input.split("\r\n")  
-    console.log(inputArray)
+    console.log(inputArray.indexOf("redis"))
     config["info"]["replication"]["master_repl_offset"]+=new TextEncoder().encode(input).byteLength
     
     // SET and GET configuration with expirity
