ad6ac2377d501adc5494a5a9bbeeb3eb43efc580
orejarenam <jmoc951@gmail.com>
2025-01-29 10:26:45 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -44,7 +44,6 @@ if(replicaofBool){
 
   master.on("data", (data)=>{
     
-    console.log(data)
     if(actualCommandIndex<3){
       return sendNextCommand(master,command)
     } 
@@ -319,7 +318,7 @@ const server = net.createServer((connection) => {
 
     // WAIT configuration
     const wait = inputArray[2] == "wait"
-    // console.log(inputArray, replicas.length)
+    console.log(inputArray, replicas.length)
     if(wait){
       return connection.write(`:${replicas.length}\r\n`)
     }
