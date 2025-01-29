ac08fec0034632b20b0c450023a26200e7f12d20
orejarenam <jmoc951@gmail.com>
2025-01-29 10:49:49 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -321,7 +321,7 @@ const server = net.createServer((connection) => {
     const wait = inputArray[2] == "wait"
     if(wait){
       setTimeout(()=>{
-        replicas[(+inputArray[4]) - 1].write("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n")
+        replicas.forEach(replica => replica.write("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n"))
       },+inputArray[6])
       
       connection.write(`:${replicas.length}\r\n`)
