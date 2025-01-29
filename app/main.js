ade095715028c1d2f687d6a3653ef10334098885
orejarenam <jmoc951@gmail.com>
2025-01-29 10:39:01 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -318,10 +318,10 @@ const server = net.createServer((connection) => {
 
     // WAIT configuration
     const wait = inputArray[2] == "wait"
-    console.log(inputArray, replicas.length, config["info"]["replication"]["master_repl_offset"])
     if(wait){
+      console.log(inputArray, replicas.length, config["info"]["replication"]["master_repl_offset"])
       setTimeout(()=>{
-        replicas[+inputArray[4]].write("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n")
+        replicas[(+inputArray[4]) - 1].write("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n")
       },+inputArray[6])
       
       connection.write(`:${replicas.length}\r\n`)
