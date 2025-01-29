5dd28d639c6de1faabb02ab4e87197bc32b56ec4
orejarenam <jmoc951@gmail.com>
2025-01-29 09:08:52 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -94,12 +94,14 @@ if(replicaofBool){
     console.log(inputArray, getackfId,"exists")
     if (getackfId!=-1){
 
+      master.write(`*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$${config["info"]["replication"]["master_repl_offset"].toString().length}\r\n${config["info"]["replication"]["master_repl_offset"]}\r\n`)
       config["info"]["replication"]["master_repl_offset"]+=37 
-      return master.write(`*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$${config["info"]["replication"]["master_repl_offset"].toString().length}\r\n${config["info"]["replication"]["master_repl_offset"]}\r\n`)
 
-    }
+    }else{
+
       // Default response to something wrong
-    return master.write('$-1\r\n') 
+      master.write('$-1\r\n') 
+    }
     
 
     
