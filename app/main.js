86e7c483471c9d33d4c042027582d15ce684506b
orejarenam <jmoc951@gmail.com>
2025-01-31 08:08:29 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -322,9 +322,9 @@ const server = net.createServer((connection) => {
     let index=0
     // WAIT configuration
     const wait = inputArray[2] == "wait"
+    console.log(inputArray)
     if(wait){
 
-      console.log(inputArray)
       replicas[+inputArray[4]-1].write("*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n")
   
       connection.write(`:${replicas.length}\r\n`)
