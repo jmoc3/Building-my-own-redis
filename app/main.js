9add98bfd02f72bc4b2e6acd552175bda376fd19
orejarenam <jmoc951@gmail.com>
2025-02-01 14:22:49 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -284,9 +284,9 @@ const server = net.createServer((connection) => {
       const bufferHeader = Buffer.from(`$${buffer.length}\r\n`)  
        
       replicas.push(connection)
+      console.log(replicas.length. replicasStorage["list"].length)
       return connection.write(Buffer.concat([bufferHeader,buffer]))
     }
-
     // ECHO configuration
     const echo = inputArray[2] == "echo"
     if(echo){
