b57b26c6e02bd34277fdf44526ab8077682e466f
orejarenam <jmoc951@gmail.com>
2025-02-02 08:38:49 -0800

    [any message]

--- app/main.js
+++ app/main.js
@@ -99,6 +99,7 @@ if(replicaofBool){
     }
     
     if (get) {
+      console.log(storage)
       if(storage[inputArray[4]]!=undefined) {
         master.write(`$${storage[inputArray[4]].value.length}\r\n${storage[inputArray[4]].value}\r\n`)
       }else{master.write("$-1\r\n")}
