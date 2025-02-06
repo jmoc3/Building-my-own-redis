9e8161621d17ce3c907f943a3684193be31c5cb5
orejarenam <jmoc951@gmail.com>
2025-02-06 08:02:21 -0800

    [any message]

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -159,7 +159,7 @@ export const commandManager = ({conn,data}) => {
     }
     const autoId = fragments[1]=="*"
     
-    if(inputArray[6]==""){
+    if(inputArray[6]=="*"){
       const unixTime = Math.floor(Date.now()/1000)
       storage[inputArray[4]] = {"value":[[`${unixTime}-0`,inputArray[8],inputArray[10]]],"expirity":"","type":"stream"}
       conn.write(`$${`${unixTime}-0`.length }\r\n${unixTime}-0\r\n`)
