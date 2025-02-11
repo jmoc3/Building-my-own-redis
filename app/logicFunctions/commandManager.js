43c1badc3a0f9abae41b5136f97f54053099eb07
orejarenam <jmoc951@gmail.com>
2025-02-11 12:51:43 -0800

    [any message]

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -230,7 +230,7 @@ export const commandManager = ({conn,data}) => {
         return [element,index]
       }
     })
-    
+    console.log(inputArray)
     values.forEach(key => {
       console.log(key[0],key[1])
       const resObject = storage[key[0]].value.filter(object => {
