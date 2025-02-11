083e0e40b31659c001e8ef1d95934faaad329d0e
orejarenam <jmoc951@gmail.com>
2025-02-11 12:58:28 -0800

    [any message]

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -231,6 +231,7 @@ export const commandManager = ({conn,data}) => {
       }
     })
     console.log(inputArray)
+    let resFormatProtocol;
     values.forEach(key => {
       console.log(key[0],key[1])
       const resObject = storage[key[0]].value.filter(object => {
@@ -243,12 +244,12 @@ export const commandManager = ({conn,data}) => {
         [`$${array[0].length}\r\n${array[0]}\r\n`, `*${array.slice(1).length}\r\n${array.slice(1).map(element => `$${element.length}\r\n${element}\r\n`).join("")}`]
       )
 
-      const resFormatProtocol = resFormat.map(array => `$${inputArray[6].length}\r\n${inputArray[6]}\r\n*1\r\n*${array.length}\r\n${array.join("")}` )
-      const res = `*1\r\n*2\r\n${resFormatProtocol.join("")}`
-
-      console.log(res,resFormatProtocol)
+      resFormatProtocol = resFormat.map(array => `$${inputArray[6].length}\r\n${inputArray[6]}\r\n*1\r\n*${array.length}\r\n${array.join("")}` )
+      
     })
     
+    const res = `*${values.length}\r\n*2\r\n${resFormatProtocol.join("")}`
+    console.log(res,resFormatProtocol)
     conn.write(res)
 
 
