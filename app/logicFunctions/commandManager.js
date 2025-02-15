b67fb68243beca3e958d898d8894860e70297bda
Orejarena <jmoc951@gmail.com>
2025-02-15 13:20:30 -0800

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -226,10 +226,10 @@ export const commandManager = ({conn,data}) => {
         time = 1000
       }
       setTimeout(()=>{
+        console.log("a")
         if(storage[inputArray[10]].value.length!=totalSpace){
           const resObject = storage[inputArray[10]].value.slice(-1)[0]
           const resFormat = `*1\r\n*2\r\n$${inputArray[10].length}\r\n${inputArray[10]}\r\n*1\r\n*2\r\n$${resObject[0].length}\r\n${resObject[0]}\r\n*2\r\n$${resObject[1].length}\r\n${resObject[1]}\r\n$${resObject[2].length}\r\n${resObject[2]}\r\n`
-          console.log("a")
           conn.write(resFormat)
         }else{
           conn.write("$-1\r\n")
