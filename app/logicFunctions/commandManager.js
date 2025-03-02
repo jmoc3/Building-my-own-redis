ec9774a2298a895ae3ad9e9ebd850a3c718e99a2
Orejarena <jmoc951@gmail.com>
2025-03-02 05:02:36 -0800

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -228,7 +228,6 @@ export const commandManager = ({conn,data}) => {
       console.log(storage[inputArray[10]])
       setTimeout(()=>{
         // Error de Tiempo, Hacer algo con la funcion xadd
-        console.log(storage[inputArray[10]])
         if(storage[inputArray[10]].value.length!=totalSpace){
           const resObject = storage[inputArray[10]].value.slice(-1)[0]
           const resFormat = `*1\r\n*2\r\n$${inputArray[10].length}\r\n${inputArray[10]}\r\n*1\r\n*2\r\n$${resObject[0].length}\r\n${resObject[0]}\r\n*2\r\n$${resObject[1].length}\r\n${resObject[1]}\r\n$${resObject[2].length}\r\n${resObject[2]}\r\n`
