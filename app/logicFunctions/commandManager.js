0e116c508e28e566050ebe6d0bfb57f5f409f19a
Orejarena <jmoc951@gmail.com>
2025-03-02 06:08:09 -0800

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -185,7 +185,7 @@ export const commandManager = ({conn,data}) => {
     
     storage[inputArray[4]].value.push([`${milliSecondsTime}-${id}`,inputArray[8],inputArray[10]])  
     conn.write(`$${`${milliSecondsTime}-${id}`.length}\r\n${milliSecondsTime}-${id}\r\n`)
-    
+    return
   }
 
   const xrange = inputArray[2]=="xrange"
