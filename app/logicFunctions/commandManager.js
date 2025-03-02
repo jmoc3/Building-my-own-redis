18e47fdc6b1fb3807f7f2e7a06d04f2aefbf30cb
Orejarena <jmoc951@gmail.com>
2025-03-02 05:08:54 -0800

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -276,7 +276,7 @@ export const commandManager = ({conn,data}) => {
   const incr = inputArray[2]=="incr"
   if(incr){
     if(!Object.keys(storage).includes(inputArray[4])){
-      storage[inputArray[4]] = {"value":inputArray[6], "expirity":"", "type":"string"}
+      storage[inputArray[4]] = {"value":"1", "expirity":"", "type":"string"}
       conn.write(":1\r\n")
       return
     }
