51304c882bbb61659aa927514b13f9c004103a32
Orejarena <jmoc951@gmail.com>
2025-03-02 07:15:28 -0800

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -190,8 +190,8 @@ export const commandManager = ({conn,data}) => {
     xaddIds[xaddIds.length-1].split("-")[0]==fragments[0] ? id=(+xaddIds[xaddIds.length-1].split("-")[1]+1) : id=id
     
     storage[inputArray[4]].value.push([`${milliSecondsTime}-${id}`,inputArray[8],inputArray[10]])  
-    console.log(storage[inputArray[4]], [`${milliSecondsTime}-${id}`,inputArray[8],inputArray[10]])
     conn.write(`$${`${milliSecondsTime}-${id}`.length}\r\n${milliSecondsTime}-${id}\r\n`)
+    console.log(storage)
     return
   }
 
