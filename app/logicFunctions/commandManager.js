f428aebb1012c90d60c2485af35e475296898882
Orejarena <jmoc951@gmail.com>
2025-03-02 06:52:42 -0800

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -241,6 +241,7 @@ export const commandManager = ({conn,data}) => {
           )
       
           const resFormatProtocol = resFormat.map(array => `*${array.length}\r\n${array.join("")}` )
+          console.log(resFormatProtocol)
           const res = `*${resFormat.length}\r\n${resFormatProtocol.join("")}`
           console.log(res)
           // const resFormat = `*1\r\n*2\r\n$${inputArray[10].length}\r\n${inputArray[10]}\r\n*1\r\n*2\r\n$${resObject[0].length}\r\n${resObject[0]}\r\n*2\r\n$${resObject[1].length}\r\n${resObject[1]}\r\n$${resObject[2].length}\r\n${resObject[2]}\r\n`
