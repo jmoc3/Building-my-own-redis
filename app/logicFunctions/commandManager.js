85151ce89f790cb60e4f3f58f5fbbad90addaf08
Orejarena <jmoc951@gmail.com>
2025-03-12 17:22:48 -0700

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -334,7 +334,8 @@ export const commandManager = ({conn,data}) => {
       return
     }
     
-    conn.write(storage['queue'])
+    storage['queue'].map(command=>console.log(`*${command.length}\r\n${command.join('\r\n')}`))
+    // conn.write(storage['queue'])
     
     storage['multi'][0]=false
     storage['queue'] = undefined
