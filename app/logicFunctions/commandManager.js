385353c03e4488e82aaab42bc5fde07688ccbd52
Orejarena <jmoc951@gmail.com>
2025-03-22 05:53:32 -0700

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -248,6 +248,7 @@ export const commandManager = ({conn,data}) => {
 
           const res = `*1\r\n*2\r\n$${inputArray[10].length}\r\n${inputArray[10]}\r\n*${resFormat.length}\r\n${resFormat.join("")}`
           conn.write(res)
+          return
         }
         return undefined
       },time)
