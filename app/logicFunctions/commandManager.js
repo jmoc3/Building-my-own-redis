0a82a24a606a8825c5a313a7efff9ad8c8010838
Orejarena <jmoc951@gmail.com>
2025-03-23 07:54:48 -0700

    test

--- app/logicFunctions/commandManager.js
+++ app/logicFunctions/commandManager.js
@@ -223,7 +223,7 @@ export const commandManager = ({conn,data}) => {
       let time = +inputArray[6]
       
       if(inputArray[6]=="0"){
-        time = 5000
+        time = 1000
       }
       
       // if(!storage[inputArray[10]]) {
